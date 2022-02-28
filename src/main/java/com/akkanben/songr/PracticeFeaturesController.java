408e76d838d09d41b4ce0ca5dd58891b2cc623d1
Ben Mills <fuzzyllama@gmail.com>
2022-02-28 15:25:34 -0800

    Finishes hello world feature on route /hello

---
+++ src/main/java/com/akkanben/songr/PracticeFeaturesController.java
@@ -0,0 +1,14 @@
+package com.akkanben.songr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PracticeFeaturesController {
+    @GetMapping("hello")
+    public String helloWorld() {
+        return "hello.html";
+    }
+
+}
