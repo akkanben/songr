78b1a6109d3dc7fb75653b4b0dde8d6b2286635e
Ben Mills <fuzzyllama@gmail.com>
2022-02-28 15:42:57 -0800

    Adds feature 3 for capitalize route

--- src/main/java/com/akkanben/songr/PracticeFeaturesController.java
+++ src/main/java/com/akkanben/songr/PracticeFeaturesController.java
@@ -1,14 +1,24 @@
 package com.akkanben.songr;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Locale;
 
 @Controller
 public class PracticeFeaturesController {
     @GetMapping("hello")
     public String helloWorld() {
         return "hello.html";
+    }
+
+    @GetMapping("capitalize/{textToCapitalize}")
+    public String capitalizeText(Model m, @PathVariable String textToCapitalize) {
+       m.addAttribute("theStringToCapitalize", textToCapitalize.toUpperCase());
+       return "capitalize.html";
     }
 
 }
