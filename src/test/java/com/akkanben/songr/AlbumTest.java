592cc972963ced1c6e2b888f562adca9e727bb07
Ben Mills <fuzzyllama@gmail.com>
2022-02-28 20:59:04 -0800

    Adds required tests for lab

---
+++ src/test/java/com/akkanben/songr/AlbumTest.java
@@ -0,0 +1,86 @@
+package com.akkanben.songr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlbumTest {
+
+    @Test
+    void test_constructor() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        assertNotNull(sut);
+    }
+
+    @Test
+    void test_gettitle() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        assertEquals(sut.getTitle(), "In League With Dragons");
+    }
+
+    @Test
+    void test_getartist() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        assertEquals(sut.getArtist(), "The Mountain Goats");
+    }
+
+    @Test
+    void test_getsongcount() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        assertEquals(sut.getSongCount(),12);
+    }
+
+    @Test
+    void test_getimageurl() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        assertEquals(sut.getImageUrl(),"https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+    }
+
+    @Test
+    void test_settitle() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        sut.setTitle("DRAGONS");
+        assertEquals(sut.getTitle(), "DRAGONS");
+    }
+
+    @Test
+    void test_setartist() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        sut.setArtist("TMG");
+        assertEquals(sut.getArtist(), "TMG");
+    }
+
+    @Test
+    void test_setsongcount() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        sut.setArtist("TMG");
+        sut.setSongCount(1);
+        assertEquals(sut.getSongCount(),1);
+    }
+
+    @Test
+    void test_setimageurl() {
+        Album sut = new Album("In League With Dragons", "The Mountain Goats",
+                12,2903,
+                "https://coverartarchive.org/release/df8b0ea3-ed60-4fa3-b3ae-d71cfa4bc238/29548494788-250.jpg");
+        sut.setImageUrl("https://potato.com");
+        sut.setSongCount(1);
+        assertEquals(sut.getImageUrl(),"https://potato.com");
+    }
+}
